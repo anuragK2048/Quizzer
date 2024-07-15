6b5ef912c788b4c6755fd9639ab0a36a1dfde998
anuragK2048 <developeranurag111@gmail.com>
2024-07-16 01:24:55 +0530

    jsonServer fix

--- src/App.js
+++ src/App.js
@@ -1,52 +1,57 @@
-import Header from "./Header"
-import Main from "./Main"
-import Loader from "./Loader"
-import Error from "./Error"
-import { useEffect, useReducer } from "react"
-import StartScreen from "./StartScreen"
-import Question from "./Question"
-import NextButton from "./Components/NextButton"
-import Timer from "./Components/Timer"
-import Footer from "./Components/Footer"
-import Progress from "./Components/Progress"
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import { useEffect, useReducer } from "react";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./Components/NextButton";
+import Timer from "./Components/Timer";
+import Footer from "./Components/Footer";
+import Progress from "./Components/Progress";
 import FinishScreen from "./Components/FinishScreen";
 
-const totalTime = {m:"10",s:"00"}
+const totalTime = { m: "10", s: "00" };
 const initialState = {
-  questions : [],
-  status:"loading",
-  index:0,
-  answer:null,
-  score:0,
-  highscore:0
-}
-
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  score: 0,
+  highscore: 0,
+};
+const jsonServer = "https://everlasting-beaded-rotate.glitch.me";
 export default function App() {
-  function reducer (state, action) {
-    switch(action.type) {
+  function reducer(state, action) {
+    switch (action.type) {
       case "dataReceived":
         return {
           ...state,
-          status : "ready",
-          questions : action.payload
-        }
+          status: "ready",
+          questions: action.payload,
+        };
       case "error":
         return {
           ...state,
-          status:"error",
-        }
+          status: "error",
+        };
       case "start":
         return {
           ...state,
-          status:"active"
-        }
+          status: "active",
+        };
       case "newAnswer":
-        const question = state.questions.at(state.index)
+        const question = state.questions.at(state.index);
         return {
-          ...state,answer:action.payload,score: action.payload === question.correctOption ? state.score + question.points : state.score
-        }
+          ...state,
+          answer: action.payload,
+          score:
+            action.payload === question.correctOption
+              ? state.score + question.points
+              : state.score,
+        };
       case "nextQuestion":
-        return {...state,index:state.index+1,answer:null}
+        return { ...state, index: state.index + 1, answer: null };
       case "finished":
         return {
           ...state,
@@ -54,36 +59,64 @@ export default function App() {
           highscore:
             state.score > state.highscore ? state.score : state.highscore,
         };
-        case "restart":
-          return { ...initialState, questions: state.questions, status: "ready", highscore: state.highscore };
-        
-        default:
-          throw new Error("Action unkonwn");
+      case "restart":
+        return {
+          ...initialState,
+          questions: state.questions,
+          status: "ready",
+          highscore: state.highscore,
+        };
+
+      default:
+        throw new Error("Action unkonwn");
     }
   }
-  const [{questions,status,index,answer,score,highscore},dispatch] = useReducer(reducer,initialState);
+  const [{ questions, status, index, answer, score, highscore }, dispatch] =
+    useReducer(reducer, initialState);
   const maxPossiblePoints = questions.reduce(
     (prev, cur) => prev + cur.points,
     0
   );
-  useEffect(function(){
-    fetch("http://localhost:8000/questions").then(res=>res.json()).then(data=>dispatch({type:"dataReceived",payload:data})).catch(err=>dispatch({type:"error",payload:`${err}`}))
-  },[])
-  return <div className="app">
-    <Header></Header>
-    <Main>
-      {status === "loading" && <Loader />}
-      {status === "ready" && <StartScreen questions={questions} dispatch={dispatch}/>}
-      {status === "error" && <Error />}
-      {status === "active" && <>
-      <Progress questions={questions} index={index} score={score} answer={answer}/>
-      <Question question={questions[index]} dispatch={dispatch} answer={answer}/>
-      <Footer>
-        <NextButton dispatch={dispatch} answer={answer} index={index} questions={questions}/>
-        <Timer totalTime={totalTime} dispatch={dispatch}/>
-      </Footer>
-      </>}
-      {status === "finished" && (
+  useEffect(function () {
+    fetch(`${jsonServer}/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "error", payload: `${err}` }));
+  }, []);
+  return (
+    <div className="app">
+      <Header></Header>
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "ready" && (
+          <StartScreen questions={questions} dispatch={dispatch} />
+        )}
+        {status === "error" && <Error />}
+        {status === "active" && (
+          <>
+            <Progress
+              questions={questions}
+              index={index}
+              score={score}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                questions={questions}
+              />
+              <Timer totalTime={totalTime} dispatch={dispatch} />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && (
           <FinishScreen
             points={score}
             maxPossiblePoints={maxPossiblePoints}
@@ -91,6 +124,7 @@ export default function App() {
             dispatch={dispatch}
           />
         )}
-    </Main>
-  </div>
+      </Main>
+    </div>
+  );
 }
